c67f623fdce930b66e866e6995b572f24a230736
inyoka <simonmarkholland@gmail.com>
2019-04-02 10:18:19 +0700

    Learn to import

---
+++ imports/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import mine
+
+# Main function that prints a greeting.
+def main():
+     print mine.hello()
+
+
+# Standard Boilerplate
+if __name__ == '__main__':
+    main()
---
+++ imports/mine.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import mine
+
+# Main function that prints a greeting.
+def hello():
+    return "Hello"
